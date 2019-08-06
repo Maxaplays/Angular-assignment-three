27ca352228dbe507e437368f5208dbb2f7f546f7
“Maxaplays” <rgmateo@hotmail.com>
2019-08-06 15:11:21 -0500

    CLOUDTAXIS-246 The Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'assignment-three';
+  btnFlag = true;
+  numberArr = [];
+  onToggleBtn() {
+    if (this.btnFlag === true) {
+      this.btnFlag = false;
+    } else {
+      this.btnFlag = true;
+    }
+  }
+  onAddNewArr() {
+    // this.numberArr.push(this.numberArr.length + 1);
+    this.numberArr.push(new Date());
+  }
 }
